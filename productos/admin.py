2f9e8efa5db2b1e8527fe95c3f7a4085199e129a
gabrielf10 <gabriel.mendo@hotmail.com>
2014-12-14 14:45:35 -0300

    cambios antes de subir  al server dreamhost

--- productos/admin.py
+++ productos/admin.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 from imagekit.admin import AdminThumbnail
 # Register your models here.
 class ImagenAdmin(admin.ModelAdmin):
-    admin_thumbnail = AdminThumbnail(image_field='imagen_miniatura')
-    list_display = ('nombre', 'categoria','admin_thumbnail')
+    imagen = AdminThumbnail(image_field='imagen_miniatura')
+    list_display = ('nombre', 'categoria','imagen')
+    
 
 admin.site.register(Categoria)
 admin.site.register(Imagen,ImagenAdmin)
--- static/js/funciones.js
+++ static/js/funciones.js
@@ -1,13 +1,40 @@
-$('.containerlittle').hide();
+$('.containerlittle').hide(0).delay(0);
 
 $(window).scroll(function (event) {
+    var position_inicio = $('#inicio').position();
+    var position_nuestrahistoria = $('#nuestrahistoria').position();
+    var position_nuestrostrabajos = $('#nuestrostrabajos').position();
+    var position_contacto = $('#contacto').position();
+
     var scroll = $(window).scrollTop();
     if (scroll > 450) {
         $('.containerlittle').show();
     } else {
         $('.containerlittle').hide();
     }
+    if(scroll < position_inicio.top -50){
+        $('.menu .selected').removeClass('selected');
+    }
+
+    if(scroll >= position_inicio.top -50){
+        $('.menu .selected').removeClass('selected');
+        $('a[href="#inicio"]').addClass('selected');
+    }
+    if(scroll >= position_nuestrahistoria.top -50){
+        $('.menu .selected').removeClass('selected');
+        $('a[href="#nuestrahistoria"]').addClass('selected');
+    }
+    if(scroll >= position_nuestrostrabajos.top -50){
+        $('.menu .selected').removeClass('selected');
+        $('a[href="#nuestrostrabajos"]').addClass('selected');
+    }
+    if(scroll >= position_contacto.top -50){
+        $('.menu .selected').removeClass('selected');
+        $('a[href="#contacto"]').addClass('selected');
+    }
 });
+
+
 $('.menu a').click(function() {
     $('.menu .selected').removeClass('selected');
     $(this).addClass('selected');
@@ -24,7 +51,12 @@ $(function () {
     $('.fotorama').fotorama();
             
     $(document).ready(function(){
+        
         $(".fotorama").hide();
+        $('#fotorama1').show();
+        $(".imgFadeIn").hide(0).fadeIn(3000);
+        $(".portada").hide(0).fadeIn(3000);
+
     });
 
     $(".fotorama-nav li").click(function(){
